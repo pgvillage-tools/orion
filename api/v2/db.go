pgvillage-tools/stolon

Working on a new v2 api

api/v2/db.go
@@ -1,0 +1,111 @@
+// Copyright 2026 PgVillage
+// Copyright 2016 Sorint.lab
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package v1
+
+import (
+	"time"
+
+	"github.com/pgvillage-tools/stolon/internal/common"
+)
+
+// DBSpec defines the spec of a database
+type DBSpec struct {
+	// The KeeperUID this db is assigned to
+	KeeperUID string `json:"keeperUID,omitempty"`
+	// Time after which any request (keepers checks from sentinel etc...) will fail.
+	RequestTimeout Duration `json:"requestTimeout,omitempty"`
+	// See Spec MaxStandbys description
+	MaxStandbys uint16 `json:"maxStandbys,omitempty"`
+	// Use Synchronous replication between master and its standbys
+	SynchronousReplication bool `json:"synchronousReplication,omitempty"`
+	// Whether to use pg_rewind
+	UsePgrewind bool `json:"usePgrewind,omitempty"`
+	// AdditionalWalSenders defines the number of additional wal_senders in
+	// addition to the ones internally defined by stolon
+	AdditionalWalSenders uint16 `json:"additionalWalSenders"`
+	// AdditionalReplicationSlots is a list of additional replication slots.
+	// Replication slots not defined here will be dropped from the instance
+	// (i.e. manually created replication slots will be removed).
+	AdditionalReplicationSlots []string `json:"additionalReplicationSlots"`
+	// InitMode defines the db initialization mode. Current modes are: none, new
+	InitMode DBInitMode `json:"initMode,omitempty"`
+	// Init configuration used when InitMode is "new"
+	NewConfig *NewConfig `json:"newConfig,omitempty"`
+	// Point in time recovery init configuration used when InitMode is "pitr"
+	PITRConfig *PITRConfig `json:"pitrConfig,omitempty"`
+	// Map of postgres parameters
+	PGParameters PGParameters `json:"pgParameters,omitempty"`
+	// Additional pg_hba.conf entries
+	// We don't set omitempty since we want to distinguish between null or empty slice
+	PGHBA []string `json:"pgHBA"`
+	// DB Role (master or standby)
+	Role common.Role `json:"role,omitempty"`
+	// FollowConfig when Role is "standby"
+	FollowConfig *FollowConfig `json:"followConfig,omitempty"`
+	// Followers DB UIDs
+	Followers []string `json:"followers"`
+	// Whether to include previous postgresql.conf
+	IncludeConfig bool `json:"includePreviousConfig,omitempty"`
+	// SynchronousStandbys are the standbys to be configured as synchronous
+	SynchronousStandbys []string `json:"synchronousStandbys"`
+	// External SynchronousStandbys are external standbys names to be configured as synchronous
+	ExternalSynchronousStandbys []string `json:"externalSynchronousStandbys"`
+}
+
+// DBStatus defines the status of a DB
+type DBStatus struct {
+	Healthy bool `json:"healthy,omitempty"`
+
+	CurrentGeneration int64 `json:"currentGeneration,omitempty"`
+
+	ListenAddress string `json:"listenAddress,omitempty"`
+	Port          string `json:"port,omitempty"`
+
+	SystemID         string                   `json:"systemdID,omitempty"`
+	TimelineID       uint64                   `json:"timelineID,omitempty"`
+	XLogPos          uint64                   `json:"xLogPos,omitempty"`
+	TimelinesHistory PostgresTimelinesHistory `json:"timelinesHistory,omitempty"`
+
+	PGParameters PGParameters `json:"pgParameters,omitempty"`
+
+	// DBUIDs of the internal standbys currently reported as in sync by the instance
+	CurSynchronousStandbys []string `json:"-"`
+
+	// DBUIDs of the internal standbys that we know are in sync.
+	// They could be currently down but we know that they were reported as in
+	// sync in the past and they are defined inside synchronous_standby_names
+	// so the instance will wait for acknowledge from them.
+	SynchronousStandbys []string `json:"synchronousStandbys"`
+
+	// NOTE(sgotti) we currently don't report the external synchronous standbys.
+	// If/when needed lets add a new ExternalSynchronousStandbys field
+
+	OlderWalFile string `json:"olderWalFile,omitempty"`
+}
+
+// DB is an instance in a cluster
+type DB struct {
+	UID        string    `json:"uid,omitempty"`
+	Generation int64     `json:"generation,omitempty"`
+	ChangeTime time.Time `json:"changeTime,omitempty"`
+
+	Spec *DBSpec `json:"spec,omitempty"`
+
+	Status DBStatus `json:"status,omitempty"`
+}
+
+// DBs can store all databases for a cluster
+type DBs map[string]*DB

api/v2/keeper.go
@@ -1,0 +1,112 @@
+// Copyright 2026 PgVillage
+// Copyright 2016 Sorint.lab
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package v1
+
+import (
+	"sort"
+	"time"
+
+	"github.com/pgvillage-tools/stolon/internal/util"
+)
+
+// KeeperSpec defines a spec for a Keeper resource
+type KeeperSpec struct{}
+
+// KeeperStatus defines all staus fields on a Keeper
+type KeeperStatus struct {
+	Healthy         bool      `json:"healthy,omitempty"`
+	LastHealthyTime time.Time `json:"lastHealthyTime,omitempty"`
+
+	BootUUID string `json:"bootUUID,omitempty"`
+
+	PostgresBinaryVersion PostgresBinaryVersion `json:"postgresBinaryVersion,omitempty"`
+
+	ForceFail bool `json:"forceFail,omitempty"`
+
+	CanBeMaster             *bool `json:"canBeMaster,omitempty"`
+	CanBeSynchronousReplica *bool `json:"canBeSynchronousReplica,omitempty"`
+}
+
+// Keeper combines the spec, status and other fields belonging to a keeper
+type Keeper struct {
+	// Keeper ID
+	UID        string    `json:"uid,omitempty"`
+	Generation int64     `json:"generation,omitempty"`
+	ChangeTime time.Time `json:"changeTime,omitempty"`
+
+	Spec *KeeperSpec `json:"spec,omitempty"`
+
+	Status KeeperStatus `json:"status,omitempty"`
+}
+
+// NewKeeperFromKeeperInfo returns a freshly initialized keeper created from a KeeperInfo object
+func NewKeeperFromKeeperInfo(ki *KeeperInfo) *Keeper {
+	return &Keeper{
+		UID:        ki.UID,
+		Generation: InitialGeneration,
+		ChangeTime: time.Time{},
+		Spec:       &KeeperSpec{},
+		Status: KeeperStatus{
+			Healthy:         true,
+			LastHealthyTime: time.Now(),
+			BootUUID:        ki.BootUUID,
+		},
+	}
+}
+
+// Keepers can store all keepers for a cluster
+type Keepers map[string]*Keeper
+
+// SortedKeys returns all keys of the Keepers as a sorted list
+func (kss Keepers) SortedKeys() []string {
+	keys := []string{}
+	for k := range kss {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+	return keys
+}
+
+// KeepersInfo stores all KeeperInfo resources belonging to this cluster
+type KeepersInfo map[string]*KeeperInfo
+
+// DeepCopy returns a copy of the KeepersInfo resource
+func (k *KeepersInfo) DeepCopy() (dc *KeepersInfo) {
+	return util.DeepCopy(k)
+}
+
+// KeeperInfo can store all info belonging to a Keeper
+type KeeperInfo struct {
+	// An unique id for this info, used to know when this the keeper info
+	// has been updated
+	InfoUID string `json:"infoUID,omitempty"`
+
+	UID        string `json:"uid,omitempty"`
+	ClusterUID string `json:"clusterUID,omitempty"`
+	BootUUID   string `json:"bootUUID,omitempty"`
+
+	PostgresBinaryVersion PostgresBinaryVersion `json:"postgresBinaryVersion,omitempty"`
+
+	PostgresState *PostgresState `json:"postgresState,omitempty"`
+
+	CanBeMaster             *bool `json:"canBeMaster,omitempty"`
+	CanBeSynchronousReplica *bool `json:"canBeSynchronousReplica,omitempty"`
+}
+
+// DeepCopy returns a copy of the KeeperInfo resource
+func (k *KeeperInfo) DeepCopy() (dc *KeeperInfo) {
+	return util.DeepCopy(k)
+}

api/v2/member.go
@@ -1,0 +1,120 @@
+// Copyright 2026 PgVillage
+// Copyright 2015 Sorint.lab
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package v1
+
+import (
+	"time"
+
+	"github.com/pgvillage-tools/stolon/internal/common"
+	"github.com/pgvillage-tools/stolon/internal/util"
+)
+
+// PostgresTimelinesHistory stores all PostgreSQL timelines belonging to this cluster
+type PostgresTimelinesHistory []*PostgresTimelineHistory
+
+// PostgresTimelineHistory defines a PostgreSQL timeline
+type PostgresTimelineHistory struct {
+	TimelineID  uint64 `json:"timelineID,omitempty"`
+	SwitchPoint uint64 `json:"switchPoint,omitempty"`
+	Reason      string `json:"reason,omitempty"`
+}
+
+// GetTimelineHistory returns a PostgresTimelineHistory for a PostgresTimelinesHistory
+func (tlsh PostgresTimelinesHistory) GetTimelineHistory(id uint64) *PostgresTimelineHistory {
+	for _, tlh := range tlsh {
+		if tlh.TimelineID == id {
+			return tlh
+		}
+	}
+	return nil
+}
+
+// PostgresState defines the state of a PostgreSQL instance
+type PostgresState struct {
+	UID        string `json:"uid,omitempty"`
+	Generation int64  `json:"generation,omitempty"`
+
+	ListenAddress string `json:"listenAddress,omitempty"`
+	Port          string `json:"port,omitempty"`
+
+	Healthy bool `json:"healthy,omitempty"`
+
+	SystemID         string                   `json:"systemID,omitempty"`
+	TimelineID       uint64                   `json:"timelineID,omitempty"`
+	XLogPos          uint64                   `json:"xLogPos,omitempty"`
+	TimelinesHistory PostgresTimelinesHistory `json:"timelinesHistory,omitempty"`
+
+	PGParameters        common.Parameters `json:"pgParameters,omitempty"`
+	SynchronousStandbys []string          `json:"synchronousStandbys"`
+	OlderWalFile        string            `json:"olderWalFile,omitempty"`
+}
+
+// DeepCopy returns a copy of the PostgresState resource
+func (p *PostgresState) DeepCopy() (dc *PostgresState) {
+	return util.DeepCopy(p)
+}
+
+// SentinelsInfo stores all SentinelInfo resources for a cluster
+type SentinelsInfo []*SentinelInfo
+
+func (s SentinelsInfo) Len() int           { return len(s) }
+func (s SentinelsInfo) Less(i, j int) bool { return s[i].UID < s[j].UID }
+func (s SentinelsInfo) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
+
+// SentinelInfo stores all info for a sentinel
+type SentinelInfo struct {
+	UID string
+}
+
+// ProxyInfo stores all info for a proxy
+type ProxyInfo struct {
+	// An unique id for this info, used to know when the proxy info
+	// has been updated
+	InfoUID string `json:"infoUID,omitempty"`
+
+	UID        string
+	Generation int64
+
+	// ProxyTimeout is the current proxyTimeout used by the proxy
+	// at the time of publishing its state.
+	// It's used by the sentinel to know for how much time the
+	// proxy should be considered active.
+	ProxyTimeout time.Duration
+}
+
+// ProxiesInfo stores inbfo about all Proxies for this cluster
+type ProxiesInfo map[string]*ProxyInfo
+
+// DeepCopy returns a copy of the ProxiesInfo resource
+func (p *ProxiesInfo) DeepCopy() (dc *ProxiesInfo) {
+	return util.DeepCopy(p)
+}
+
+// ToSlice converts the ProxiesInfo map into a slice of ProxyInfo resources
+func (p ProxiesInfo) ToSlice() ProxiesInfoSlice {
+	pis := ProxiesInfoSlice{}
+	for _, pi := range p {
+		pis = append(pis, pi)
+	}
+	return pis
+}
+
+// ProxiesInfoSlice defines a slice of ProxyInfo resources
+type ProxiesInfoSlice []*ProxyInfo
+
+func (p ProxiesInfoSlice) Len() int           { return len(p) }
+func (p ProxiesInfoSlice) Less(i, j int) bool { return p[i].UID < p[j].UID }
+func (p ProxiesInfoSlice) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }

pgvillage-tools/orion

api/v2/db.go
@@ -1,0 +1,111 @@
+// Copyright 2026 PgVillage
+// Copyright 2016 Sorint.lab
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package v1
+
+import (
+	"time"
+
+	"github.com/pgvillage-tools/orion/internal/common"
+)
+
+// DBSpec defines the spec of a database
+type DBSpec struct {
+	// The KeeperUID this db is assigned to
+	KeeperUID string `json:"keeperUID,omitempty"`
+	// Time after which any request (keepers checks from sentinel etc...) will fail.
+	RequestTimeout Duration `json:"requestTimeout,omitempty"`
+	// See Spec MaxStandbys description
+	MaxStandbys uint16 `json:"maxStandbys,omitempty"`
+	// Use Synchronous replication between master and its standbys
+	SynchronousReplication bool `json:"synchronousReplication,omitempty"`
+	// Whether to use pg_rewind
+	UsePgrewind bool `json:"usePgrewind,omitempty"`
+	// AdditionalWalSenders defines the number of additional wal_senders in
+	// addition to the ones internally defined by stolon
+	AdditionalWalSenders uint16 `json:"additionalWalSenders"`
+	// AdditionalReplicationSlots is a list of additional replication slots.
+	// Replication slots not defined here will be dropped from the instance
+	// (i.e. manually created replication slots will be removed).
+	AdditionalReplicationSlots []string `json:"additionalReplicationSlots"`
+	// InitMode defines the db initialization mode. Current modes are: none, new
+	InitMode DBInitMode `json:"initMode,omitempty"`
+	// Init configuration used when InitMode is "new"
+	NewConfig *NewConfig `json:"newConfig,omitempty"`
+	// Point in time recovery init configuration used when InitMode is "pitr"
+	PITRConfig *PITRConfig `json:"pitrConfig,omitempty"`
+	// Map of postgres parameters
+	PGParameters PGParameters `json:"pgParameters,omitempty"`
+	// Additional pg_hba.conf entries
+	// We don't set omitempty since we want to distinguish between null or empty slice
+	PGHBA []string `json:"pgHBA"`
+	// DB Role (master or standby)
+	Role common.Role `json:"role,omitempty"`
+	// FollowConfig when Role is "standby"
+	FollowConfig *FollowConfig `json:"followConfig,omitempty"`
+	// Followers DB UIDs
+	Followers []string `json:"followers"`
+	// Whether to include previous postgresql.conf
+	IncludeConfig bool `json:"includePreviousConfig,omitempty"`
+	// SynchronousStandbys are the standbys to be configured as synchronous
+	SynchronousStandbys []string `json:"synchronousStandbys"`
+	// External SynchronousStandbys are external standbys names to be configured as synchronous
+	ExternalSynchronousStandbys []string `json:"externalSynchronousStandbys"`
+}
+
+// DBStatus defines the status of a DB
+type DBStatus struct {
+	Healthy bool `json:"healthy,omitempty"`
+
+	CurrentGeneration int64 `json:"currentGeneration,omitempty"`
+
+	ListenAddress string `json:"listenAddress,omitempty"`
+	Port          string `json:"port,omitempty"`
+
+	SystemID         string                   `json:"systemdID,omitempty"`
+	TimelineID       uint64                   `json:"timelineID,omitempty"`
+	XLogPos          uint64                   `json:"xLogPos,omitempty"`
+	TimelinesHistory PostgresTimelinesHistory `json:"timelinesHistory,omitempty"`
+
+	PGParameters PGParameters `json:"pgParameters,omitempty"`
+
+	// DBUIDs of the internal standbys currently reported as in sync by the instance
+	CurSynchronousStandbys []string `json:"-"`
+
+	// DBUIDs of the internal standbys that we know are in sync.
+	// They could be currently down but we know that they were reported as in
+	// sync in the past and they are defined inside synchronous_standby_names
+	// so the instance will wait for acknowledge from them.
+	SynchronousStandbys []string `json:"synchronousStandbys"`
+
+	// NOTE(sgotti) we currently don't report the external synchronous standbys.
+	// If/when needed lets add a new ExternalSynchronousStandbys field
+
+	OlderWalFile string `json:"olderWalFile,omitempty"`
+}
+
+// DB is an instance in a cluster
+type DB struct {
+	UID        string    `json:"uid,omitempty"`
+	Generation int64     `json:"generation,omitempty"`
+	ChangeTime time.Time `json:"changeTime,omitempty"`
+
+	Spec *DBSpec `json:"spec,omitempty"`
+
+	Status DBStatus `json:"status,omitempty"`
+}
+
+// DBs can store all databases for a cluster
+type DBs map[string]*DB

api/v2/keeper.go
@@ -1,0 +1,112 @@
+// Copyright 2026 PgVillage
+// Copyright 2016 Sorint.lab
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package v1
+
+import (
+	"sort"
+	"time"
+
+	"github.com/pgvillage-tools/orion/internal/util"
+)
+
+// KeeperSpec defines a spec for a Keeper resource
+type KeeperSpec struct{}
+
+// KeeperStatus defines all staus fields on a Keeper
+type KeeperStatus struct {
+	Healthy         bool      `json:"healthy,omitempty"`
+	LastHealthyTime time.Time `json:"lastHealthyTime,omitempty"`
+
+	BootUUID string `json:"bootUUID,omitempty"`
+
+	PostgresBinaryVersion PostgresBinaryVersion `json:"postgresBinaryVersion,omitempty"`
+
+	ForceFail bool `json:"forceFail,omitempty"`
+
+	CanBeMaster             *bool `json:"canBeMaster,omitempty"`
+	CanBeSynchronousReplica *bool `json:"canBeSynchronousReplica,omitempty"`
+}
+
+// Keeper combines the spec, status and other fields belonging to a keeper
+type Keeper struct {
+	// Keeper ID
+	UID        string    `json:"uid,omitempty"`
+	Generation int64     `json:"generation,omitempty"`
+	ChangeTime time.Time `json:"changeTime,omitempty"`
+
+	Spec *KeeperSpec `json:"spec,omitempty"`
+
+	Status KeeperStatus `json:"status,omitempty"`
+}
+
+// NewKeeperFromKeeperInfo returns a freshly initialized keeper created from a KeeperInfo object
+func NewKeeperFromKeeperInfo(ki *KeeperInfo) *Keeper {
+	return &Keeper{
+		UID:        ki.UID,
+		Generation: InitialGeneration,
+		ChangeTime: time.Time{},
+		Spec:       &KeeperSpec{},
+		Status: KeeperStatus{
+			Healthy:         true,
+			LastHealthyTime: time.Now(),
+			BootUUID:        ki.BootUUID,
+		},
+	}
+}
+
+// Keepers can store all keepers for a cluster
+type Keepers map[string]*Keeper
+
+// SortedKeys returns all keys of the Keepers as a sorted list
+func (kss Keepers) SortedKeys() []string {
+	keys := []string{}
+	for k := range kss {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+	return keys
+}
+
+// KeepersInfo stores all KeeperInfo resources belonging to this cluster
+type KeepersInfo map[string]*KeeperInfo
+
+// DeepCopy returns a copy of the KeepersInfo resource
+func (k *KeepersInfo) DeepCopy() (dc *KeepersInfo) {
+	return util.DeepCopy(k)
+}
+
+// KeeperInfo can store all info belonging to a Keeper
+type KeeperInfo struct {
+	// An unique id for this info, used to know when this the keeper info
+	// has been updated
+	InfoUID string `json:"infoUID,omitempty"`
+
+	UID        string `json:"uid,omitempty"`
+	ClusterUID string `json:"clusterUID,omitempty"`
+	BootUUID   string `json:"bootUUID,omitempty"`
+
+	PostgresBinaryVersion PostgresBinaryVersion `json:"postgresBinaryVersion,omitempty"`
+
+	PostgresState *PostgresState `json:"postgresState,omitempty"`
+
+	CanBeMaster             *bool `json:"canBeMaster,omitempty"`
+	CanBeSynchronousReplica *bool `json:"canBeSynchronousReplica,omitempty"`
+}
+
+// DeepCopy returns a copy of the KeeperInfo resource
+func (k *KeeperInfo) DeepCopy() (dc *KeeperInfo) {
+	return util.DeepCopy(k)
+}

api/v2/member.go
@@ -1,0 +1,120 @@
+// Copyright 2026 PgVillage
+// Copyright 2015 Sorint.lab
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package v1
+
+import (
+	"time"
+
+	"github.com/pgvillage-tools/orion/internal/common"
+	"github.com/pgvillage-tools/orion/internal/util"
+)
+
+// PostgresTimelinesHistory stores all PostgreSQL timelines belonging to this cluster
+type PostgresTimelinesHistory []*PostgresTimelineHistory
+
+// PostgresTimelineHistory defines a PostgreSQL timeline
+type PostgresTimelineHistory struct {
+	TimelineID  uint64 `json:"timelineID,omitempty"`
+	SwitchPoint uint64 `json:"switchPoint,omitempty"`
+	Reason      string `json:"reason,omitempty"`
+}
+
+// GetTimelineHistory returns a PostgresTimelineHistory for a PostgresTimelinesHistory
+func (tlsh PostgresTimelinesHistory) GetTimelineHistory(id uint64) *PostgresTimelineHistory {
+	for _, tlh := range tlsh {
+		if tlh.TimelineID == id {
+			return tlh
+		}
+	}
+	return nil
+}
+
+// PostgresState defines the state of a PostgreSQL instance
+type PostgresState struct {
+	UID        string `json:"uid,omitempty"`
+	Generation int64  `json:"generation,omitempty"`
+
+	ListenAddress string `json:"listenAddress,omitempty"`
+	Port          string `json:"port,omitempty"`
+
+	Healthy bool `json:"healthy,omitempty"`
+
+	SystemID         string                   `json:"systemID,omitempty"`
+	TimelineID       uint64                   `json:"timelineID,omitempty"`
+	XLogPos          uint64                   `json:"xLogPos,omitempty"`
+	TimelinesHistory PostgresTimelinesHistory `json:"timelinesHistory,omitempty"`
+
+	PGParameters        common.Parameters `json:"pgParameters,omitempty"`
+	SynchronousStandbys []string          `json:"synchronousStandbys"`
+	OlderWalFile        string            `json:"olderWalFile,omitempty"`
+}
+
+// DeepCopy returns a copy of the PostgresState resource
+func (p *PostgresState) DeepCopy() (dc *PostgresState) {
+	return util.DeepCopy(p)
+}
+
+// SentinelsInfo stores all SentinelInfo resources for a cluster
+type SentinelsInfo []*SentinelInfo
+
+func (s SentinelsInfo) Len() int           { return len(s) }
+func (s SentinelsInfo) Less(i, j int) bool { return s[i].UID < s[j].UID }
+func (s SentinelsInfo) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
+
+// SentinelInfo stores all info for a sentinel
+type SentinelInfo struct {
+	UID string
+}
+
+// ProxyInfo stores all info for a proxy
+type ProxyInfo struct {
+	// An unique id for this info, used to know when the proxy info
+	// has been updated
+	InfoUID string `json:"infoUID,omitempty"`
+
+	UID        string
+	Generation int64
+
+	// ProxyTimeout is the current proxyTimeout used by the proxy
+	// at the time of publishing its state.
+	// It's used by the sentinel to know for how much time the
+	// proxy should be considered active.
+	ProxyTimeout time.Duration
+}
+
+// ProxiesInfo stores inbfo about all Proxies for this cluster
+type ProxiesInfo map[string]*ProxyInfo
+
+// DeepCopy returns a copy of the ProxiesInfo resource
+func (p *ProxiesInfo) DeepCopy() (dc *ProxiesInfo) {
+	return util.DeepCopy(p)
+}
+
+// ToSlice converts the ProxiesInfo map into a slice of ProxyInfo resources
+func (p ProxiesInfo) ToSlice() ProxiesInfoSlice {
+	pis := ProxiesInfoSlice{}
+	for _, pi := range p {
+		pis = append(pis, pi)
+	}
+	return pis
+}
+
+// ProxiesInfoSlice defines a slice of ProxyInfo resources
+type ProxiesInfoSlice []*ProxyInfo
+
+func (p ProxiesInfoSlice) Len() int           { return len(p) }
+func (p ProxiesInfoSlice) Less(i, j int) bool { return p[i].UID < p[j].UID }
+func (p ProxiesInfoSlice) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }